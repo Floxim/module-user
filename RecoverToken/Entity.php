9e6be94d995efe64f5a6e99449ece61ea6c95d8b
dubr <dubr.cola@gmail.com>
2017-03-13 10:18:09 +0300

    recovery token check

--- RecoverToken/Entity.php
+++ RecoverToken/Entity.php
@@ -5,5 +5,13 @@ use Floxim\Floxim\System\Fx as fx;
 
 class Entity extends \Floxim\Floxim\Component\Basic\Entity
 {
-
+    public function isExpired()
+    {
+        $expire_date = $this['expire_date'];
+        if (!$expire_date) {
+            return false;
+        }
+        $expire_time = fx::date($expire_date, 'U')*1;
+        return $expire_time < time();
+    }
 }
